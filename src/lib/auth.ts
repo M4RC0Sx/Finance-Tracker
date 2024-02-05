4988caeb0aa861deeab575463760c4f0b61f5506
M4RC0Sx <marcos3as3@gmail.com>
2024-02-05 21:51:58 +0100

    feat(auth): set up prisma adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
 import type { NextAuthConfig } from "next-auth";
+
+const prisma = new PrismaClient();
 
 export const config: NextAuthConfig = {
   theme: {
     logo: "https://next-auth.js.org/img/logo/logo-sm.png",
   },
+  adapter: PrismaAdapter(prisma),
   providers: [
     github({
       clientId: process.env.AUTH_GITHUB_ID,
